9f57edddd91c22dc630e621f6e25aecccfd35fc2
Dion Tu <diontu1@gmail.com>
2019-03-02 13:37:41 -0500

    implemented array based stack

--- Data Structures/src/stacks/ArrayBasedStack.java
+++ Data Structures/src/stacks/ArrayBasedStack.java
@@ -1,5 +1,42 @@
 package stacks;
 
-public class ArrayBasedStack {
-
+public class ArrayBasedStack <E> {
+	
+	E [] stack;
+	int size;
+	
+	public ArrayBasedStack () {
+		stack = (E[]) new Object[2];
+		size = 0;
+	}
+	
+	public void push(E e) {
+		if (size == stack.length) {
+			E [] newStack = (E[]) new Object[size*2];
+			for (int i=0; i < size; i++) {
+				newStack[i] = stack[i];
+			}
+			stack = newStack;
+		}
+		stack[size] = e;
+		size++;
+	}
+	
+	public E pop() {
+		if (size == 0) {
+			return null;
+		}
+		size --;
+		E temp = stack[size];
+		stack[size] = null;
+		return temp;
+	}
+	
+	public E peek() {
+		if (size == 0) {
+			return null;
+		}
+		return stack[size-1];
+		
+	}
 }
